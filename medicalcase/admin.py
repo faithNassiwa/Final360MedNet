4edc2191340b935a4aacf3315ce16b06bdd49779
faithNassiwa <faithnassiwa@gmail.com>
2017-10-28 14:47:46 +0300

    fix for medical cases not appearing in django admin

--- medicalcase/admin.py
+++ medicalcase/admin.py
@@ -3,15 +3,16 @@ from .models import MedicalCase, MedicalCaseCategory, Comment, Reply, Photo
 
 
 class MedicalCaseAdmin(admin.ModelAdmin):
+
     list_display = ['id', 'title', 'chief_complaint', 'patient_age', 'patient_gender', 'patient_country_of_origin',
                     'history_of_present_illness', 'medical_history', 'surgical_history', 'social_history',
-                    'family_history','allergies', 'medications', 'review_of_systems', 'physical_examination',
+                    'family_history', 'allergies', 'medications', 'review_of_systems', 'physical_examination',
                     'diagnostic_tests', 'medical_case_categories', 'created_at', 'updated_at', 'doctor']
     list_filter = ['created_at', 'updated_at', ]
     search_fields = ['id', 'doctor']
 
     def medical_case_categories(self, obj):
-        return ",".join([medical_case_category.name for medical_case_category in obj.MedicalCaseCategory.all()])
+        return ",".join([medical_case_category.name for medical_case_category in obj.medical_case_category.all()])
 
 
 admin.site.register(MedicalCase, MedicalCaseAdmin)
